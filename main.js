ef4e16d6cf707cd20c62b3a6261cf5fbe53f6445
esterbvila <ester.batllori@gmail.com>
2022-01-02 20:29:48 +0100

    completed level-1

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product:'Socks',
+            description:'The best socks of the market'
+        }
+    }
+})
